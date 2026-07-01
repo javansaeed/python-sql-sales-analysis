753ebb3bb3141c17b32eac7a6b74432efc727e73
javansaeed <javansaeed@hotmail.com>
2026-07-01 17:41:57 +0330

    Update config

--- src/config.py
+++ src/config.py
@@ -1,1 +1,20 @@
+import os
 
+# =========================
+# SQL Server Configuration
+# =========================
+
+SERVER = r"localhost\MSSQLSERVER25"
+DATABASE = "TSQLV4"
+
+# =========================
+# Project Paths
+# =========================
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+OUTPUT_DIR = os.path.join(BASE_DIR, "..", "output")
+
+CSV_DIR = os.path.join(OUTPUT_DIR, "csv")
+CHART_DIR = os.path.join(OUTPUT_DIR, "charts")
+REPORT_DIR = os.path.join(OUTPUT_DIR, "reports")
