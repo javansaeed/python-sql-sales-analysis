4b3c2fd3ae08da544b20ea838e5aaf5f8f6d07f8
javansaeed <javansaeed@hotmail.com>
2026-07-01 21:03:25 +0330

    Add functions to save bar and line charts

---
+++ src/charts.py
@@ -0,0 +1,38 @@
+import os
+import matplotlib.pyplot as plt
+
+
+def save_bar_chart(series, title, xlabel, ylabel, path):
+    plt.figure(figsize=(10, 5))
+
+    series.plot(kind="bar")
+
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    plt.xticks(rotation=45)
+
+    plt.tight_layout()
+
+    plt.savefig(path, dpi=300, bbox_inches="tight")
+
+    plt.show()
+
+
+def save_line_chart(x, y, title, xlabel, ylabel, path):
+    plt.figure(figsize=(12, 5))
+
+    plt.plot(x, y, marker="o")
+
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    plt.xticks(rotation=45)
+
+    plt.tight_layout()
+
+    plt.savefig(path, dpi=300, bbox_inches="tight")
+
+    plt.show()
